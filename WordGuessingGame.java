94f823f7cf4c4e35ea9e090ef3233fa90a3287b1
RodrigoNunes-202100224 <202100224@estudantes.ips.pt>
2024-03-14 10:28:22 +0000

    Nível 1

---
+++ WordGuessingGame.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class WordGuessingGame here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class WordGuessingGame
+{
+    private String hiddenWord;
+    private String guessedWord;
+    private int numberOfTries;
+    public WordGuessingGame()
+    {
+        this.hiddenWord = "abc";
+        this.guessedWord = "Word";
+        this.numberOfTries = 0;
+    }
+
+    public String getHiddenWord(){
+        return hiddenWord;
+    }
+
+    public String guessedWord(){
+        return guessedWord;
+    }
+
+    public int numberOfTries(){
+        return numberOfTries;
+    }
+
+    public void showGuessedWord(){
+        System.out.println(guessedWord);
+    }
+}
